c0e364a0362b8ff6aa12fd3634ad8327d7081c2d
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2024-11-27 18:43:54 -0800

    file selection

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QtWidgets>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,5 +12,21 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButtonFileSelection_clicked()
+{
+    QFileDialog dialog;
+    dialog.setFileMode(QFileDialog::ExistingFile);
+    dialog.setNameFilter("Bitmap files (*.bmp);;All files (*.*)");
+
+    if (dialog.exec() == QDialog::Accepted)
+    {
+        QString fileName = dialog.selectedFiles().first();
+        // Do something with the selected file
+        QMessageBox::information(this, "File selection", fileName);
+        qDebug() << "Selected file:" << fileName;
+    }
 }
 
